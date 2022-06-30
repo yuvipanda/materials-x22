9a1129992bb63ac47b72e61548d9cc0e5aea1b42
Sean Morris <sean.smorris@berkeley.edu>
2022-06-30 10:20:45 -0700

    Fixed Section 3 Lab 1

--- materials/x22/lab/3/lab01/tests/q2_1.py
+++ materials/x22/lab/3/lab01/tests/q2_1.py
@@ -1,6 +1,6 @@
 test = {   'name': 'q2_1',
     'points': None,
-    'suites': [   {   'cases': [{'code': ">>> np.random.seed(8)\n>>> np.round(one_sample_mean(salaries, 'salary', 100), 3) == 76699.829\n", 'hidden': False, 'locked': False}],
+    'suites': [   {   'cases': [{'code': ">>> np.random.seed(8)\n>>> np.round(one_sample_mean(salaries, 'salary', 100), 3) == 76699.829\nTrue", 'hidden': False, 'locked': False}],
                       'scored': True,
                       'setup': '',
                       'teardown': '',
