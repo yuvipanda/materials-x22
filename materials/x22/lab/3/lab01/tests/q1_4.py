390b75a40a54de2b24beec2529408d6edb41a829
Sean Morris <sean.smorris@berkeley.edu>
2022-06-30 10:18:19 -0700

    Add Section 3 Lab 1

---
+++ materials/x22/lab/3/lab01/tests/q1_4.py
@@ -0,0 +1,7 @@
+test = {   'name': 'q1_4',
+    'points': None,
+    'suites': [   {   'cases': [{'code': '>>> round(height_mean, 3) == 64.049\n', 'hidden': False, 'locked': False}, {'code': '>>> round(height_sd, 3) == 2.525\n', 'hidden': False, 'locked': False}],
+                      'scored': True,
+                      'setup': '',
+                      'teardown': '',
+                      'type': 'doctest'}]}
