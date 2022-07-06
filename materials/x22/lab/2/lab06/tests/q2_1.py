4bd833f36fb40cb102e7762a56b0df5b066ae0e4
Sean Morris <sean.smorris@berkeley.edu>
2022-07-05 18:45:01 -0700

    Added Section 2 Lab 6

---
+++ materials/x22/lab/2/lab06/tests/q2_1.py
@@ -0,0 +1,8 @@
+test = {   'name': 'q2_1',
+    'points': [0, 1],
+    'suites': [   {   'cases': [   {'code': '>>> totals.num_rows == 61\n', 'hidden': False, 'locked': False},
+                                   {'code': ">>> totals.labels == ('Year', 'Precipitation')\n", 'hidden': False, 'locked': False}],
+                      'scored': True,
+                      'setup': '',
+                      'teardown': '',
+                      'type': 'doctest'}]}
---
+++ materials/x22/lab/2/lab06/tests/q2_8.py
@@ -0,0 +1,8 @@
+test = {   'name': 'q2_8',
+    'points': [0, 0],
+    'suites': [   {   'cases': [   {'code': '>>> len(sampled_stats) == 5000\nFalse', 'hidden': False, 'locked': False},
+                                   {'code': '>>> np.std(sampled_stats) > 0\nFalse', 'hidden': False, 'locked': False}],
+                      'scored': True,
+                      'setup': '',
+                      'teardown': '',
+                      'type': 'doctest'}]}
