a4eeb91f4d7db69ac79e40c5290af025a5b4c92e
Sean Morris <sean.smorris@berkeley.edu>
2022-07-06 11:12:14 -0700

    Added Section 3 Lab 3

---
+++ materials/x22/lab/3/lab03/tests/q2_1_1.py
@@ -0,0 +1,1 @@
+test = {'name': 'q2_1_1', 'points': [0], 'suites': [{'cases': [{'code': '>>> 0 < one_distance < .01\n', 'hidden': False, 'locked': False}], 'scored': True, 'setup': '', 'teardown': '', 'type': 'doctest'}]}
