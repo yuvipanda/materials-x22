fb4f9b98ea7a7930518c7d73de99073c2af03f06
Sean Morris <sean.smorris@berkeley.edu>
2022-07-06 11:09:29 -0700

    Fixed Section 3 Lab 2

--- materials/x22/lab/3/lab02/tests/q3_1_2.py
+++ materials/x22/lab/3/lab02/tests/q3_1_2.py
@@ -6,7 +6,8 @@ test = {   'name': 'q3_1_2',
                                                '...     """Helper function for assessing correctness."""\n'
                                                '...     return hashlib.md5(str(num).encode()).hexdigest()\n'
                                                '>>> \n'
-                                               '>>> get_hash(np.round(probability_positive_test, 4))\n',
+                                               '>>> get_hash(np.round(probability_positive_test, 4))\n'
+                                               "'c50fc42a07a3cc2af3249a6e3ed78374'",
                                        'hidden': False,
                                        'locked': False}],
                       'scored': True,
