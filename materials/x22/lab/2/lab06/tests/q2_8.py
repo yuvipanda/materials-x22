0a0549512645ba780f088ea8f8d8297878ce2986
Sean Morris <sean.smorris@berkeley.edu>
2022-07-05 18:49:47 -0700

    Fixed Section 2 Lab 6

--- materials/x22/lab/2/lab06/tests/q2_8.py
+++ materials/x22/lab/2/lab06/tests/q2_8.py
@@ -1,7 +1,6 @@
 test = {   'name': 'q2_8',
     'points': [0, 0],
-    'suites': [   {   'cases': [   {'code': '>>> len(sampled_stats) == 5000\nFalse', 'hidden': False, 'locked': False},
-                                   {'code': '>>> np.std(sampled_stats) > 0\nFalse', 'hidden': False, 'locked': False}],
+    'suites': [   {   'cases': [{'code': '>>> len(sampled_stats) == 5000\nTrue', 'hidden': False, 'locked': False}, {'code': '>>> np.std(sampled_stats) > 0\nTrue', 'hidden': False, 'locked': False}],
                       'scored': True,
                       'setup': '',
                       'teardown': '',
