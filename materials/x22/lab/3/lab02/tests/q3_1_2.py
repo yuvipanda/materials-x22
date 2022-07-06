f4223b237f8dac4336e600f74a15fcc1a860d11c
Sean Morris <sean.smorris@berkeley.edu>
2022-07-06 11:04:04 -0700

    Add Section 3 Lab 2

---
+++ materials/x22/lab/3/lab02/tests/q3_1_2.py
@@ -0,0 +1,15 @@
+test = {   'name': 'q3_1_2',
+    'points': None,
+    'suites': [   {   'cases': [   {   'code': '>>> \n'
+                                               '>>> import hashlib \n'
+                                               '>>> def get_hash(num):\n'
+                                               '...     """Helper function for assessing correctness."""\n'
+                                               '...     return hashlib.md5(str(num).encode()).hexdigest()\n'
+                                               '>>> \n'
+                                               '>>> get_hash(np.round(probability_positive_test, 4))\n',
+                                       'hidden': False,
+                                       'locked': False}],
+                      'scored': True,
+                      'setup': '',
+                      'teardown': '',
+                      'type': 'doctest'}]}
