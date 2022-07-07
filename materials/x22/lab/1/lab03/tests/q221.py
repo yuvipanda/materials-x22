1a78c930e5ef6ca5ab49bf71abec12d75f572b74
Sean Morris <sean.smorris@berkeley.edu>
2022-07-06 17:31:23 -0700

    Fixes to labs after Data 8 generation

--- materials/x22/lab/1/lab03/tests/q221.py
+++ materials/x22/lab/1/lab03/tests/q221.py
@@ -1,1 +1,1 @@
-test = {'name': 'q221', 'points': None, 'suites': [{'cases': [{'code': '>>> population_1973\n', 'hidden': False, 'locked': False}], 'scored': True, 'setup': '', 'teardown': '', 'type': 'doctest'}]}
+test = {'name': 'q221', 'points': None, 'suites': [{'cases': [{'code': '>>> population_1973\n3942096442', 'hidden': False, 'locked': False}], 'scored': True, 'setup': '', 'teardown': '', 'type': 'doctest'}]}
--- materials/x22/lab/1/lab04/tests/q22.py
+++ materials/x22/lab/1/lab04/tests/q22.py
@@ -1,1 +1,3 @@
-test = {'name': 'q22', 'points': None, 'suites': [{'cases': [{'code': '>>> str(mark_hurd_pay_string)\n', 'hidden': False, 'locked': False}], 'scored': True, 'setup': '', 'teardown': '', 'type': 'doctest'}]}
+test = {   'name': 'q22',
+    'points': None,
+    'suites': [{'cases': [{'code': ">>> str(mark_hurd_pay_string)\n'$53.25 '", 'hidden': False, 'locked': False}], 'scored': True, 'setup': '', 'teardown': '', 'type': 'doctest'}]}
