49ad4af39f78baf8e923921840387f4495f07894
Sean Morris <sean.smorris@berkeley.edu>
2022-07-28 18:45:58 -0700

    Section 2: Lab 5 and 6
    - Hidden Tests to Test

--- materials/x22/lab/2/lab06/tests/q2_7.py
+++ materials/x22/lab/2/lab06/tests/q2_7.py
@@ -1,3 +1,8 @@
 test = {   'name': 'q2_7',
-    'points': [1],
-    'suites': [{'cases': [{'code': '>>> -50 < simulate_precipitation_null() < 50\nTrue', 'hidden': False, 'locked': False}], 'scored': True, 'setup': '', 'teardown': '', 'type': 'doctest'}]}
+    'points': None,
+    'suites': [   {   'cases': [   {'code': '>>> -50 < simulate_precipitation_null() < 50\nTrue', 'hidden': False, 'locked': False},
+                                   {'code': '>>> ts = [simulate_precipitation_null() for _ in range(1000)]\n>>> 6 <= np.array(ts).std() <= 10\nTrue', 'hidden': False, 'locked': False}],
+                      'scored': True,
+                      'setup': '',
+                      'teardown': '',
+                      'type': 'doctest'}]}
